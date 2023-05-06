a1c3257cd31269ee54a2daaaedcb7a5683307074
Dipankar <dipankar15-11137@diu.edu.bd>
2023-05-06 10:12:53 +0600

    manage booking

--- src/Components/Pages/Dashboard/Boooking/Booking.js
+++ src/Components/Pages/Dashboard/Boooking/Booking.js
@@ -1,10 +1,27 @@
 import React from "react";
 
-const Booking = () => {
+const Booking = ({ booking, index, handleDelete }) => {
+  const { _id, name, shipCode, terminalName, date, slot, phone, description } =
+    booking;
   return (
-    <div>
-      <h1>Booking</h1>
-    </div>
+    <tr>
+      <th>{index}</th>
+      <td>{name}</td>
+      <td>{shipCode}</td>
+      <td>{terminalName}</td>
+      <td>{date}</td>
+      <td>{slot}</td>
+      <td>{phone}</td>
+      <td>{description}</td>
+      <td>
+        <button
+          onClick={() => handleDelete(_id)}
+          className="bg-blue-800 px-3 py-1 rounded-md uppercase text-white font-semibold hover:bg-blue-600"
+        >
+          delivered
+        </button>
+      </td>
+    </tr>
   );
 };
 
--- src/Components/Pages/Dashboard/Boooking/Bookings.js
+++ src/Components/Pages/Dashboard/Boooking/Bookings.js
@@ -1,9 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Booking from "./Booking";
 
 const Bookings = () => {
+  const [bookings, setBooking] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/bookings")
+      .then((res) => res.json())
+      .then((data) => setBooking(data));
+  }, [bookings]);
+
+  const handleDelete = (id) => {
+    console.log(id);
+  };
   return (
-    <div>
-      <h1>Bookings</h1>
+    <div className="">
+      <h1 className="text-2xl font-semibold text-center py-5">
+        Manage All Booking
+      </h1>
+      <div className="overflow-x-auto">
+        <table className="table  w-full text-black">
+          <thead>
+            <tr className="text-3xl">
+              <th></th>
+              <th>Ship Name</th>
+              <th>Ship Code</th>
+              <th>Terminal</th>
+              <th>Date</th>
+              <th>Slot</th>
+              <th>Phone</th>
+              <th>Description</th>
+              <th>Delivery</th>
+            </tr>
+          </thead>
+          <tbody>
+            {bookings.map((booking, index) => (
+              <Booking
+                key={booking._id}
+                booking={booking}
+                index={index + 1}
+                handleDelete={handleDelete}
+              ></Booking>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
